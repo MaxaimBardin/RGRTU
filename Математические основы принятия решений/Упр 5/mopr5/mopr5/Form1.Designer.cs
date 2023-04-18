5bfa6203b96530e44f657d9e529a23ea34cf4e53
Max Bardin <bmaximss2003@gmail.com>
2023-04-18 07:20:05 +0300

    123

---
+++ Математические основы принятия решений/Упр 5/mopr5/mopr5/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿
+namespace mopr5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewPareto = new System.Windows.Forms.DataGridView();
+            this.dataGridViewPareto1 = new System.Windows.Forms.DataGridView();
+            this.Button_Pareto = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ButtonArray = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column19 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column20 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column21 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column22 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPareto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPareto1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewPareto
+            // 
+            this.dataGridViewPareto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPareto.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column13,
+            this.Column15,
+            this.Column16,
+            this.Column19,
+            this.Column20});
+            this.dataGridViewPareto.Location = new System.Drawing.Point(10, 9);
+            this.dataGridViewPareto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridViewPareto.Name = "dataGridViewPareto";
+            this.dataGridViewPareto.RowHeadersWidth = 51;
+            this.dataGridViewPareto.RowTemplate.Height = 29;
+            this.dataGridViewPareto.Size = new System.Drawing.Size(1341, 376);
+            this.dataGridViewPareto.TabIndex = 0;
+            // 
+            // dataGridViewPareto1
+            // 
+            this.dataGridViewPareto1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPareto1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column14,
+            this.Column17,
+            this.Column18,
+            this.Column21,
+            this.Column22});
+            this.dataGridViewPareto1.Location = new System.Drawing.Point(10, 403);
+            this.dataGridViewPareto1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridViewPareto1.Name = "dataGridViewPareto1";
+            this.dataGridViewPareto1.RowHeadersWidth = 51;
+            this.dataGridViewPareto1.RowTemplate.Height = 29;
+            this.dataGridViewPareto1.Size = new System.Drawing.Size(1341, 360);
+            this.dataGridViewPareto1.TabIndex = 1;
+            // 
+            // Button_Pareto
+            // 
+            this.Button_Pareto.Location = new System.Drawing.Point(1059, 798);
+            this.Button_Pareto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Button_Pareto.Name = "Button_Pareto";
+            this.Button_Pareto.Size = new System.Drawing.Size(82, 22);
+            this.Button_Pareto.TabIndex = 2;
+            this.Button_Pareto.Text = "Вычислить";
+            this.Button_Pareto.UseVisualStyleBackColor = true;
+            this.Button_Pareto.Click += new System.EventHandler(this.Button_Pareto_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 777);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 798);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 798);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Индекс";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 777);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Вектор";
+            // 
+            // ButtonArray
+            // 
+            this.ButtonArray.Location = new System.Drawing.Point(894, 798);
+            this.ButtonArray.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ButtonArray.Name = "ButtonArray";
+            this.ButtonArray.Size = new System.Drawing.Size(159, 22);
+            this.ButtonArray.TabIndex = 7;
+            this.ButtonArray.Text = "Перезаписать массив";
+            this.ButtonArray.UseVisualStyleBackColor = true;
+            this.ButtonArray.Click += new System.EventHandler(this.ButtonArray_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(806, 798);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 22);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Массив";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Вариант №1";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Вариант №2";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Вариант №3";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Вариант №4";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Вариант №5";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 125;
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "Вариант №6";
+            this.Column13.MinimumWidth = 6;
+            this.Column13.Name = "Column13";
+            this.Column13.Width = 125;
+            // 
+            // Column15
+            // 
+            this.Column15.HeaderText = "Вариант №7";
+            this.Column15.Name = "Column15";
+            // 
+            // Column16
+            // 
+            this.Column16.HeaderText = "Вариант №8";
+            this.Column16.Name = "Column16";
+            // 
+            // Column19
+            // 
+            this.Column19.HeaderText = "Вариант №9";
+            this.Column19.Name = "Column19";
+            // 
+            // Column20
+            // 
+            this.Column20.HeaderText = "Вариант №10";
+            this.Column20.Name = "Column20";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 125;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Вариант №1";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 125;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Вариант №2";
+            this.Column9.MinimumWidth = 6;
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 125;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Вариант №3";
+            this.Column10.MinimumWidth = 6;
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 125;
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "Вариант №4";
+            this.Column11.MinimumWidth = 6;
+            this.Column11.Name = "Column11";
+            this.Column11.Width = 125;
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "Вариант №5";
+            this.Column12.MinimumWidth = 6;
+            this.Column12.Name = "Column12";
+            this.Column12.Width = 125;
+            // 
+            // Column14
+            // 
+            this.Column14.HeaderText = "Вариант №6";
+            this.Column14.MinimumWidth = 6;
+            this.Column14.Name = "Column14";
+            this.Column14.Width = 125;
+            // 
+            // Column17
+            // 
+            this.Column17.HeaderText = "Вариант №7";
+            this.Column17.Name = "Column17";
+            // 
+            // Column18
+            // 
+            this.Column18.HeaderText = "Вариант №8";
+            this.Column18.Name = "Column18";
+            // 
+            // Column21
+            // 
+            this.Column21.HeaderText = "Вариант №9";
+            this.Column21.Name = "Column21";
+            // 
+            // Column22
+            // 
+            this.Column22.HeaderText = "Вариант №10";
+            this.Column22.Name = "Column22";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1420, 831);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ButtonArray);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Button_Pareto);
+            this.Controls.Add(this.dataGridViewPareto1);
+            this.Controls.Add(this.dataGridViewPareto);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPareto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPareto1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewPareto;
+        private System.Windows.Forms.DataGridView dataGridViewPareto1;
+        private System.Windows.Forms.Button Button_Pareto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button ButtonArray;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column17;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column18;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column19;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column20;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column21;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column22;
+    }
+}
+
---
+++ Математические основы принятия решений/Упр 5/mopr5/mopr5/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace mopr5
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        double[,] myArr = new double[10, 10];
+        double[,] myArr1 = new double[10, 10];
+
+        public Form1()
+        {
+            InitializeComponent();
+            GenArray();
+            dataGridViewPareto.RowCount = 10;
+            dataGridViewPareto1.RowCount = 10;
+            for (int i = 0; i < 10; i++)
+            {
+                dataGridViewPareto.Rows[i].Cells[0].Value = "Вариант N" + Convert.ToString(i + 1);
+                dataGridViewPareto1.Rows[i].Cells[0].Value = "Вариант N" + Convert.ToString(i + 1);
+            }
+            ShowArray();
+            ShowArray1();
+        }
+        public void ShowArray() // Отображение массива в таблице
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    dataGridViewPareto.Rows[i].Cells[j + 1].Value = myArr[i, j];
+                }
+            }
+        }
+
+        public void ShowArray1() // Отображение массива в таблице
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    dataGridViewPareto1.Rows[i].Cells[j + 1].Value = myArr[i, j];
+                }
+            }
+        }
+
+        public void GenArray() // Генерация массива случайных значений
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i == j)
+                    {
+                        myArr[i, j] = 1.0;
+                    }
+                    else
+                    if (i > j)
+                    {
+                        myArr[i, j] = Math.Round(1.0 / Convert.ToDouble(myArr[j, i]), 3);
+                    }
+                    else
+                    {
+                        myArr[i, j] = rand.Next(1, 6);
+                    }
+                }
+            }
+        }
+
+        public void ReadArray() // Чтение массива с таблицы при изменении значений массива
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    myArr1[i, j] = Convert.ToInt32(dataGridViewPareto.Rows[i].Cells[j + 1].Value);
+                    if (i == j)
+                        myArr1[i, j] = 1.0;
+                }
+            }
+        }
+
+        private double[] NormalMatrix(double[,] matrix)
+        {
+            var sum = new double[10];
+            var norm_v = new double[10];
+            double summ = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum[i] = 1;
+                for (int j = 0; j < 10; j++)
+                {
+                    sum[i] = matrix[i, j];
+                }
+                sum[i] = Math.Pow(sum[i], 1/10.0);//оценки компомент собственого вектора
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                summ += sum[i];//сумма оценки компонент собственого вектора
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                norm_v[i] = sum[i]/summ;//Нормализованые оценки вектора приоритета
+            }
+            return norm_v;
+        }
+
+        public double MaxZnM(double[,] matrix, double[] norm_v)
+        {
+            var sum = new double[10];
+            var lmax = new double[10];
+            double lmaxx = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    sum[i] += matrix[j, i];//сумма столбцов
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                lmax[i] = sum[i]* norm_v[i];
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                lmaxx += lmax[i];//максимальное собственное значение
+            }
+            return lmaxx;
+        }
+
+        private void Button_Pareto_Click(object sender, EventArgs e)  //Реализация МАИ
+        {
+            ReadArray();
+            label1.Text = "";
+            label2.Text = "";
+            var n = 10;
+            ShowArray();
+            var norm_v = NormalMatrix(myArr); //Нормализованные оценки вектора приоритета
+            var lmax = MaxZnM(myArr, norm_v); //Максимальное собственное значение
+            ShowArray1();
+
+            for (int c = 0; c < 10; c++)
+                label1.Text += Convert.ToString(Math.Round(norm_v[c], 2)) + " ";
+
+            var IC = (lmax - n) / (n - 1); //индекс согласованности
+            var CC = 1.49; //значение случайной согласованности для матрицы 6-го порядка
+            var OC = IC / CC; //оценка согласованности матрицы
+
+            label2.Text = Convert.ToString(Math.Round(OC, 2));
+            if (OC <= 0.1)
+                label2.Text += " - матрица согласованная";
+            else
+                label2.Text += " - матрица не согласованная";
+        }
+
+        private void ButtonArray_Click(object sender, EventArgs e) //генерация и вывод массива случайных значений
+        {
+            GenArray();
+            ShowArray();
+            ShowArray1();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReadArray();
+            ShowArray1();
+        }
+    }
+}
+
+       
