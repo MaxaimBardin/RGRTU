b6f378cb95d159642c95995fea1fbcb76bc79700
Max <bmaximss2003@gmail.com>
2023-05-17 18:59:32 +0300

    Говно

--- Дискретная математика/Lab_2/Lab2DinamicProg/Lab2DinamicProg/Program.cs
+++ Дискретная математика/Lab_2/Lab2DinamicProg/Lab2DinamicProg/Program.cs
@@ -1,33 +1,100 @@
-﻿namespace Lab2DinamicProg
+﻿using System;
+using System.Collections.Generic;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int n = 7; // Количество вершин в графе
+
+        int[,] adjacencyMatrix = new int[,]
         {
-            Console.WriteLine("Hello, World!");
+            { 0, 0, 0, 0, 0, 0, 0 }, // Вершина 0
+            { 3, 0, 0, 0, 0, 0, 0 }, // Вершина 1
+            { 2, 4, 0, 0, 0, 0, 0 }, // Вершина 2
+            { 0, 1, 0, 0, 0, 0, 0 }, // Вершина 3
+            { 0, 0, 2, 5, 0, 0, 0 }, // Вершина 4
+            { 0, 0, 0, 1, 0, 0, 0 }, // Вершина 5
+            { 0, 0, 0, 0, 2, 1, 0 }  // Вершина 6
+        };
+
+        int source = 1; // Исходная вершина (a)
+        int destination = 6; // Целевая вершина (b)
+
+        List<int> sortedVertices = TopologicalSort(adjacencyMatrix, n);
+        Console.WriteLine("Отсортированные вершины:");
+        foreach (int vertex in sortedVertices)
+        {
+            Console.Write(vertex + " ");
         }
 
-        public static int[,] InputMatr(int[,] matr)
-        {
-            int[,] mat = new int[,] {{0, 8, 11, 0, 0, 0, 0 },
-                                     { 0, 0, 10, 9, 7, 12, 0},
-                                     { 0, 0, 0, 3, 0, 0, 5},
-                                     {0, 0, 0, 0, 2, 0, 0},
-                                     {0, 0, 0, 0, 0, 8, 4},
-                                     { 0, 0, 0, 0, 0, 0, 6},
-                                     { 0, 0, 0, 0, 0, 0, 0}};
+        Console.WriteLine();
 
-            return matr;
-        }
+        int shortestDistance = FindShortestDistance(adjacencyMatrix, sortedVertices, source, destination);
+        Console.WriteLine("Минимальное расстояние между вершинами {0} и {1}: {2}", source, destination, shortestDistance);
+    }
 
-        public static int[,] OutputMatr(int[,] matr, int lenght, int hight)
+    static List<int> TopologicalSort(int[,] adjacencyMatrix, int n)
+    {
+        List<int> sortedVertices = new List<int>();
+        bool[] visited = new bool[n];
+
+        for (int i = 0; i < n; i++)
         {
-            for (var x = 1; x < lenght; x++)
+            if (!visited[i])
             {
-                for (var y = 1; y < hight)
+                DFS(adjacencyMatrix, i, visited, sortedVertices);
             }
-
-            return matr;
         }
+
+        sortedVertices.Reverse();
+        return sortedVertices;
+    }
+
+    static void DFS(int[,] adjacencyMatrix, int vertex, bool[] visited, List<int> sortedVertices)
+    {
+        visited[vertex] = true;
+
+        for (int i = 0; i < adjacencyMatrix.GetLength(1); i++)
+        {
+            if (adjacencyMatrix[vertex, i] > 0 && !visited[i])
+            {
+                DFS(adjacencyMatrix, i, visited, sortedVertices);
+            }
+        }
+
+        sortedVertices.Add(vertex);
+    }
+
+    static int FindShortestDistance(int[,] adjacencyMatrix, List<int> sortedVertices, int source, int destination)
+    {
+        int n = adjacencyMatrix.GetLength(0);
+        int[] distances = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            distances[i] = int.MaxValue;
+        }
+
+        distances[source] = 0;
+
+        foreach (int vertex in sortedVertices)
+        {
+            if (distances[vertex] != int.MaxValue)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (adjacencyMatrix[vertex, i] > 0) // Проверяем, что есть ребро
+                    {
+                        int newDistance = distances[vertex] + adjacencyMatrix[vertex, i];
+                        if (newDistance < distances[i])
+                        {
+                            distances[i] = newDistance;
+                        }
+                    }
+                }
+            }
+        }
+
+        return distances[destination];
     }
 }
