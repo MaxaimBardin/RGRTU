f4d2f330e1523effc438f5ed72552cce804e14aa
Бардин Максим Сергеевич <105451157+MaximBardinGod@users.noreply.github.com>
2023-05-30 14:20:32 +0300

    lab 1 and lab 3 update

---
+++ Дискретная математика/Lab_1/Lab_1_v1.0/Lab_1_v1.0.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int N, M;
+int main() {
+	setlocale(LC_CTYPE, "Russian");
+	const int N = 6;
+	const int M = 9;
+	int f, d;
+	double matr1[N][M];
+	double matr[N][N]
+	{
+	{0, 1, 1, 0, 0, 1 },
+	{1, 0, 1, 1, 0, 1 },
+	{1, 1, 0, 0, 1, 0 },
+	{0, 1, 0, 0, 1, 1 },
+	{0, 0, 1, 1, 0, 0 },
+	{1, 1, 0, 1, 0, 0 },
+	};
+	cout << "Преобразованная матрица смежности:" << endl;
+	for (f = 0; f < 6; f++)
+	{
+		for (d = 0; d < 6; d++)
+		{
+			if ((matr[f][d] == 1) && (matr[f][d] == matr[d][f]))
+			{
+				matr[d][f] = 0;
+			}
+			cout << matr[f][d] << "  ";
+		}
+		cout << endl;
+	};
+	cout << endl;
+	int h = 0;
+	int m = 0;
+	for (f = 0; f < 6; f++)
+	{
+		for (d = 0; d < 6; d++)
+		{
+			if (matr[f][d] == 1)
+			{
+				for (m = 0; m < 6; m++)
+				{
+					matr1[m][h] = 0;
+				}
+				matr1[f][h] = 1;
+				matr1[d][h] = 1;
+				h++;
+			}
+		}
+	}
+	cout << "Матрица инцидентности:" << endl;
+	for (f = 0; f < 6; f++)
+	{
+		for (d = 0; d < 9; d++)
+		{
+			cout << matr1[f][d] << "  ";
+		}
+		cout << endl;
+	};
+	cout << endl;
+}
